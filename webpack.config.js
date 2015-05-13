56b5287773ae3327cd077456a1d41fd5d2771cb1
Richard Goater <richard.goater27@gmail.com>
2015-05-13 16:07:04 +0100

    Add `dist` folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   context: path.join(__dirname, 'src'),
   entry: './index',
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'dist'),
+    publicPath: 'dist/',
     filename: 'phylocanvas.js',
     library: 'PhyloCanvas',
     libraryTarget: 'umd'
