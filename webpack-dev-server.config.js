f2871bb702acc9c2983cc148284b7b50d5577310
Richard Goater <richard.goater27@gmail.com>
2015-06-02 14:03:13 +0100

    generate sourcemaps

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   context: path.join(__dirname, 'dev'),
-  devtool: 'eval',
+  devtool: '#eval-source-map',
   debug: true,
   entry: './index',
   module: {
