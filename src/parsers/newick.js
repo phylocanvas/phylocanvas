ac6617e66884b387d6c5539ae1836bd2f9c44b90
Jyothish <joyjoy.nt@gmail.com>
2015-06-18 10:14:03 +0100

    Fix #127 parser fix on rooted trees

--- src/parsers/newick.js
+++ src/parsers/newick.js
@@ -10,11 +10,10 @@ function isTerminatingChar(terminatingChar) {
   return this === terminatingChar;
 }
 
-const labelTerminatingChars = [ ':', ',', ')' ];
+const labelTerminatingChars = [ ':', ',', ')', ';' ];
 
 function parseLabel(string) {
   let label = '';
-
   for (let char of string) {
     if (labelTerminatingChars.some(isTerminatingChar.bind(char))) {
       return label;
@@ -65,22 +64,19 @@ const nodeTerminatingChars = [ ')', ',' ];
 
 function parseBranchLength(string) {
   let nodeLength = '';
-
   for (let char of string) {
     if (nodeTerminatingChars.some(isTerminatingChar.bind(char))) {
       break;
     }
     nodeLength += char;
   }
-  return nodeLength;
+  return parseFloat(nodeLength)?nodeLength:0.0;
 }
-
 
 function parseBranch(branch, string, index) {
   let label = parseLabel(string.slice(index));
   let postLabelIndex = index + label.length;
-  let branchLengthStr = 0;
-
+  let branchLengthStr = '';
   if (label.match(/\*/)) {
     parseAnnotations(label, branch);
   }
@@ -92,13 +88,15 @@ function parseBranch(branch, string, index) {
     branch.branchLength = 0;
   }
 
+  if (label) {
+    branch.label = label;
+  }
   branch.id = label || branch.tree.generateBranchId();
   return postLabelIndex + branchLengthStr.length;
 }
 
 function parseFn({ string, root }, callback) {
   let currentNode = root;
-
   for (let i = 0; i < string.length; i++) {
     let node;
     switch (string[i]) {
@@ -116,7 +114,7 @@ function parseFn({ string, root }, callback) {
         currentNode = node;
         break;
       case ';':
-        return callback();
+        break;
       default:
           try {
             i = parseBranch(currentNode, string, i);
@@ -126,6 +124,7 @@ function parseFn({ string, root }, callback) {
         break;
     }
   }
+  callback();
 }
 
 export default {
