a67cc828c08e25207dbf866ced438926eab40a1d
Richard Goater <richard.goater27@gmail.com>
2015-08-08 12:11:54 +0100

    Remove plugins from core library

--- dev/index.js
+++ dev/index.js
@@ -1,11 +1,4 @@
 import PhyloCanvas, * as phyloComponents from '../src/index';
-import historyPlugin from '../src/plugins/History';
-import ajaxPlugin from '../src/plugins/Ajax';
-import metadataPlugin from '../src/plugins/Metadata';
-
-PhyloCanvas.plugin(historyPlugin);
-PhyloCanvas.plugin(ajaxPlugin);
-PhyloCanvas.plugin(metadataPlugin);
 
 let buttonForm = document.getElementById('buttons');
 let tree = PhyloCanvas.createTree('phylocanvas', {
@@ -41,23 +34,17 @@ tree.on('error', function (event) { throw event.error; });
 
 tree.on('loaded', function () {
   console.log('loaded');
-  for (let i = 0; i <= 12; i++) {
-    if (tree.leaves[i]) {
-      tree.leaves[i].data = { col: 1, x: 0, a: 1, c: 0 };
-    }
-  }
-  tree.viewMetadataColumns();
 });
 
 tree.alignLabels = true;
 tree.setTreeType('circular');
 
-tree.load('./data/tree.nwk', function () {
+// ./data/tree.nwk
+// (A:0.1,B:0.1,(C:0.1,D:0.1):0.1);
+tree.load('(A:0.1,B:0.1,(C:0.1,D:0.1):0.1);', function () {
   tree.backColour = true;
   tree.setNodeSize(5);
   tree.setNodeDisplay('B', { colour: 'red', shape: 'triangle' });
   tree.setNodeDisplay('C', { colour: 'blue', shape: 'star' });
   tree.setNodeDisplay('D', { colour: 'green' });
 });
-// tree.load('./data/tree.nwk', {}, () => { console.log('callback!'); });
-// tree.load('(A:0.1,B:0.1,(C:0.1,D:0.1):0.1);');
